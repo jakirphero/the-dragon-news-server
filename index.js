783e6c9c10a32ffe64cef8ffb76fe7f766efb762
jakir <mdjakirhossain2235@gmail.com>
2023-08-13 11:21:16 +0600

    add another data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var cors = require('cors')
 app.use(cors());
 
 const categorizes = require('./data/categorizes.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('Dragon is running!')
@@ -15,6 +16,27 @@ app.get('/categorizes', (req, res) => {
     res.send(categorizes);
 })
 
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectNews = news.find(n => n._id === id);
+    res.send(selectNews);
+})
+app.get('/categorizes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categorizesNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categorizesNews);
+    }
+
+})
 app.listen(port, () => {
     console.log(`Dragon API running on port ${port}`)
 })
